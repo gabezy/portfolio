a30300849b4404d0f4fa17d1dd535e4144f2f944
gabezy <gabrielmoreira2705@gmail.com>
2022-11-27 00:38:42 -0300

    set form contact

---
+++ js/modules/form.js
@@ -0,0 +1,31 @@
+export default function initFormContanct() {
+  const form = document.querySelector('[data-form="form"]');
+
+  if (form) {
+    form.addEventListener("submit", sendForm);
+
+    function sentForm(response) {
+      if (response.ok) {
+        form.innerHTML =
+          "<p style='padding-block: 1rem; grid-column: 1/-1; justify-self: center;'><span style='color: var(--clr-typewriter);'>Messange sent successfully</span>.<br> i will contact you soon</p>";
+      } else {
+        form.innerHTML =
+          "<p style='padding-block: 1rem; grid-column: 1/-1; justify-self: center; margin-bottom: 0.5rem'>Erro sending email, you can send directly for my email:<span style='color: var; display: block;(--clr-typewriter);'>contact@gabrielmdev.com</span></p>";
+      }
+    }
+
+    function sendForm(e) {
+      e.preventDefault();
+      const btn = document.querySelector('[data-form="btn"]');
+      btn.disabled = true;
+      btn.innerText = "Sending...";
+
+      const data = new FormData(form);
+
+      fetch("../../send.php", {
+        method: "POST",
+        body: data,
+      }).then(sentForm);
+    }
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 import initMobileNav from "./modules/mobileMenu.js";
 import initAnimatedScroll from "./modules/animateScroll.js";
 import initTypewriter from "./modules/typewriter.js";
+import initFormContanct from "./modules/form.js";
 
 initMobileNav();
 initTypewriter();
 initAnimatedScroll();
+initFormContanct();
 
 // // Dark Mode
 
